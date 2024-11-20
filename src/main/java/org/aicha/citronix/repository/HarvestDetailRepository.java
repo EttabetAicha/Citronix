3992ba1c25581c5b1d11c5620cffb5870e77946c
ettabet aicha <aicha70tabite@gmail.com>
2024-11-20 23:56:05 +0100

    end manage harvest details

--- src/main/java/org/aicha/citronix/repository/HarvestDetailRepository.java
+++ src/main/java/org/aicha/citronix/repository/HarvestDetailRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface HarvestDetailRepository extends JpaRepository<HarvestDetail, Integer> {
 
     @Query("SELECT hd FROM HarvestDetail hd WHERE hd.tree.id = :treeId")
-    List<HarvestDetail> findByTreeId(Integer treeId);
+    List<HarvestDetail> findByHarvestId(UUID harvestId);
+
+    // Method to find HarvestDetails by Tree ID
+    List<HarvestDetail> findByTreeId(UUID treeId);
 }
