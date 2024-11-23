95d22cba7c15da41889f5ac3b34c94d1fa31e3de
ettabet aicha <aicha70tabite@gmail.com>
2024-11-23 11:54:36 +0100

    model refactor

--- src/main/java/org/aicha/citronix/domain/Field.java
+++ src/main/java/org/aicha/citronix/domain/Field.java
@@ -1,8 +1,8 @@
 package org.aicha.citronix.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
 import lombok.*;
 
 import java.util.List;
@@ -16,43 +16,25 @@ import java.util.UUID;
 @Entity
 public class Field {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private UUID id;
 
-    @NotNull
-    private String name;
+    @Column(nullable = false)
+    @Min(value = 1000, message = "Area must be at least 1000 m²")
+    private double area;
 
-    @NotNull
-    @DecimalMin(value = "0.1", message = "The area must be at least 0.1 hectare")
-    private Double area;
-
-    @ManyToOne
-    @JoinColumn(name = "farm_id")
+    @ManyToOne(optional = false)
     private Farm farm;
 
-    @OneToMany(mappedBy = "field", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "field")
+    @JsonIgnoreProperties({"field"})
     private List<Tree> trees;
-    @Override
-    public String toString() {
-        return "Field{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", area=" + area +
-                ", farm=" + farm +
-                '}';
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
 
-        Field field = (Field) o;
-
-        return id != null ? id.equals(field.id) : field.id == null;
-    }
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
+    public boolean isTreeDensityValid(int numberOfTrees) {
+        return numberOfTrees <= area * 10;
     }
 
+    public boolean isAreaValid() {
+        return area < (farm.getArea() * 0.5);
+    }
 }
--- src/main/java/org/aicha/citronix/domain/Tree.java
+++ src/main/java/org/aicha/citronix/domain/Tree.java
@@ -1,13 +1,10 @@
 package org.aicha.citronix.domain;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.aicha.citronix.domain.enums.Status;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.List;
 import java.util.UUID;
 
 @Setter
@@ -18,35 +15,32 @@ import java.util.UUID;
 @Entity
 public class Tree {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private UUID id;
 
-    @NotNull
-    private LocalDate plantationDate;
+    @Column(nullable = false)
+    private LocalDate plantingDate;
 
-    @Enumerated(EnumType.STRING)
-    @NotNull
-    private Status status;
-
-    @ManyToOne
-    @JoinColumn(name = "field_id")
+    @ManyToOne(optional = false)
     private Field field;
 
-    @OneToMany(mappedBy = "tree", cascade = CascadeType.ALL)
-    private List<HarvestDetail> harvestDetails;
 
-    public int getAge() {
-        return Period.between(this.plantationDate, LocalDate.now()).getYears();
+    public int calculateAge() {
+        return Period.between(plantingDate, LocalDate.now()).getYears();
     }
 
-    public double getProductivity() {
-        int age = getAge();
-        if (age < 3) {
-            return 2.5;
-        } else if (age <= 10) {
-            return 12.0;
-        } else {
-            return 20.0;
+    public double calculateProductivity() {
+        int age = calculateAge();
+        if (age > 20) return 0.0;
+        if (age < 3) return 2.5;
+        else if (age <= 10) return 12.0;
+        else return 20.0;
+    }
+
+    public void validatePlantingDate() {
+        int plantingMonth = plantingDate.getMonthValue();
+        if (plantingMonth < 3 || plantingMonth > 5) {
+            throw new IllegalArgumentException("Trees can only be planted between March and May.");
         }
     }
 }
